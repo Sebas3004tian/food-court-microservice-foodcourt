f349596aec5278350c9655db7fc90d55b64292cb
Sebastian Lopez <sebas3004tian@gmail.com>
2026-03-24 16:57:29 -0500

    chore: added initial project structure

---
+++ src/main/java/com/foodcourt/food_court_microservice_foodcourt/FoodCourtMicroserviceFoodcourtApplication.java
@@ -0,0 +1,13 @@
+package com.foodcourt.food_court_microservice_foodcourt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCourtMicroserviceFoodcourtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodCourtMicroserviceFoodcourtApplication.class, args);
+	}
+
+}
