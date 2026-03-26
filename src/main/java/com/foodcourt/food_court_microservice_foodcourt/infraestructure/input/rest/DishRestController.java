7aec3b89ac55a12bbe13f86c87ba9fdc8d5438ab
Sebastian Lopez <sebas3004tian@gmail.com>
2026-03-26 11:27:06 -0500

    docs

--- src/main/java/com/foodcourt/food_court_microservice_foodcourt/infraestructure/input/rest/DishRestController.java
+++ src/main/java/com/foodcourt/food_court_microservice_foodcourt/infraestructure/input/rest/DishRestController.java
@@ -3,6 +3,9 @@ package com.foodcourt.food_court_microservice_foodcourt.infraestructure.input.re
 import com.foodcourt.food_court_microservice_foodcourt.application.dto.request.CreateDishRequestDto;
 import com.foodcourt.food_court_microservice_foodcourt.application.dto.request.UpdateDishRequestDto;
 import com.foodcourt.food_court_microservice_foodcourt.application.handler.IDishHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,6 +22,13 @@ public class DishRestController {
 
     @PreAuthorize("hasAnyRole('ADMIN', 'PROPIETARIO')")
     @PostMapping("/")
+    @Operation(summary = "Create an dish")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Dish created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data"),
+            @ApiResponse(responseCode = "403", description = "Access Denied"),
+            @ApiResponse(responseCode = "409", description = "Dish already exists")
+    })
     public ResponseEntity<Void> createDish(@Valid @RequestBody CreateDishRequestDto createDishRequestDto){
         dishHandler.createDish(createDishRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
@@ -26,6 +36,12 @@ public class DishRestController {
 
     @PreAuthorize("hasAnyRole('ADMIN', 'PROPIETARIO')")
     @PutMapping("/{dishId}")
+    @Operation(summary = "Update an dish")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dish updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data"),
+            @ApiResponse(responseCode = "403", description = "Access Denied")
+    })
     public ResponseEntity<Void> updateDish(@PathVariable Long dishId, @Valid @RequestBody UpdateDishRequestDto updateDishRequestDto){
         dishHandler.updateDish(dishId, updateDishRequestDto);
         return ResponseEntity.ok().build();
@@ -33,6 +49,12 @@ public class DishRestController {
 
     @PreAuthorize("hasAnyRole('ADMIN', 'PROPIETARIO')")
     @PatchMapping("/{dishId}/active")
+    @Operation(summary = "Enable or disable  dish status")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dish status changed successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid data"),
+            @ApiResponse(responseCode = "403", description = "Access Denied")
+    })
     public ResponseEntity<Void> enableOrDisableDish(@PathVariable Long dishId, @RequestParam boolean active) {
         dishHandler.enableOrDisableDish(dishId, active);
         return ResponseEntity.ok().build();
